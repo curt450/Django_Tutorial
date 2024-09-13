9a9fc7eeae8b869f48fe91d3574568bd187b5e9e
curt450 <curtdelong429@gmail.com>
2024-09-13 18:55:10 -0400

    CRUD

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.home, name="home"), 
     path('room/<str:pk>/', views.room, name="room"),
 
+    path('create-room/', veiws.createRoom, name="create-room"),
+    path('update-room/<str:pk>/', veiws.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', veiws.deleteRoom, name="delete-room"),
 ]
