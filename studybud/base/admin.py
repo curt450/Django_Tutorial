2787a4ee5abae512bff2ba3016f025a47c64a1a4
curt450 <curtdelong429@gmail.com>
2024-09-13 18:07:09 -0400

    database and admin panel

--- studybud/base/admin.py
+++ studybud/base/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Room, Topic, Message
 
-admin.site.Register(Room)
-admin.site.Register(Messages)
-admin.site.Register(Topic)
+admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(Topic)
