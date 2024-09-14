a55abf3ed6388f7732c7e5d13fa02b1b6515022e
curt450 <curtdelong429@gmail.com>
2024-09-14 02:36:34 -0400

    Search

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
-
+from django.db.models import Q
 # Create your views here.
 
 
@@ -13,8 +13,18 @@ from .forms import RoomForm
 # ]
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get() != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | 
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+    
+
+    topics = topic.objects.all()
+    room_count = rooms.count()
+
+    context = {'rooms': rooms, 'topics':topics, 'room_count': room_count}
     return  render(request, 'base/home.html', context)
 
 def room(request, pk):
