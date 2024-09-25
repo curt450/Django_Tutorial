b04a3c91b2ba854e22ee8cf29e0019e78953de63
curt450 <curtdelong429@gmail.com>
2024-09-25 00:04:23 -0400

    Django REST Framework

--- studybud/base/forms.py
+++ studybud/base/forms.py
@@ -1,14 +1,22 @@
-from django.forms import modelForm
-from .models import Room
-from django.contrib.auth.models import User
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room, User
 
-class RoomForm(modelForm):
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['name', 'username', 'email', 'password1', 'password2']
+
+
+class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
         exclude = ['host', 'participants']
 
 
-class UserForm(modelForm):
+class UserForm(ModelForm):
     class Meta:
-        model = ['username, email']
+        model = User
+        fields = ['avatar', 'name', 'username', 'email', 'bio']
