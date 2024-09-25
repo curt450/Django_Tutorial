21b8ed2afdf3624c0551c2de9399a55b5cef85aa
curt450 <curtdelong429@gmail.com>
2024-09-24 23:40:09 -0400

    Edit User Account pages

--- studybud/base/forms.py
+++ studybud/base/forms.py
@@ -1,9 +1,14 @@
 from django.forms import modelForm
 from .models import Room
-
+from django.contrib.auth.models import User
 
 class RoomForm(modelForm):
     class Meta:
         model = Room
         fields = '__all__'
         exclude = ['host', 'participants']
+
+
+class UserForm(modelForm):
+    class Meta:
+        model = ['username, email']
